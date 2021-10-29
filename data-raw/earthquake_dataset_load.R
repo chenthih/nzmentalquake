dc67bcff11993d49a5ee93415072b38778ee9490
chenthih <chenthiheer@gmail.com>
2021-10-30 09:33:07 +1300

    fixed column titles

--- data-raw/earthquake_dataset_load.R
+++ data-raw/earthquake_dataset_load.R
@@ -1,5 +1,6 @@
 # create_package(getwd())
 setwd("~/OneDrive - University of Canterbury/DATA201/nzmentalquake/data-raw")
-earthquake_dataset <- read.csv("earthquake.csv")
 
-usethis::use_data(earthquake_dataset, overwrite = TRUE)
+earthquake_summary_dataset <- read.csv("earthquake.csv")
+
+usethis::use_data(earthquake_summary_dataset, overwrite = TRUE)
