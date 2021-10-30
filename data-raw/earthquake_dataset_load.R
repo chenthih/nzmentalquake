094929172b73e535e94c2190e0eaf24833b368dc
chenthih <chenthiheer@gmail.com>
2021-10-31 11:27:51 +1300

    fixed eq summary dataset

--- data-raw/earthquake_dataset_load.R
+++ data-raw/earthquake_dataset_load.R
@@ -4,3 +4,4 @@ setwd("~/OneDrive - University of Canterbury/DATA201/nzmentalquake/data-raw")
 earthquake_summary_dataset <- read.csv("earthquake.csv")
 
 usethis::use_data(earthquake_summary_dataset, overwrite = TRUE)
+nz
